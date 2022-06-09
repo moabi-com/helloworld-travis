439383bfed7b68522424784eaf15c6afca5271de
Moabi <48871061+moabi-com@users.noreply.github.com>
2022-06-09 22:12:34 +0200

    Create hello.c
    Initial hello.c commit.

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* hello.c : v0.1 : Tue 08 Feb 2022 08:12:28 AM CET
+* Jonathan Brossard // jonathan.brossard@moabi.com
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <errno.h>
+
+int main (int argc, char **argv, char **envp)
+{
+	printf("Hello World!\n");
+	return 0;
+}
